a28bf6c25fc1c3a9552d6784d47c2d4cd55d5b57
alirezamhm <alireza.am1379@gmail.com>
2020-08-24 18:17:38 +0430

    create password hash and log in

--- src/safe.py
+++ src/safe.py
@@ -1,4 +1,20 @@
 from getpass import getpass
+import hashlib
 
-pas = getpass("Please Enter Your password: ")
-print(pas)
+
+hashed_psw = ''
+psw = ''
+try:
+    with open('.password.txt', 'r') as pswfile:
+        hashed_psw = pswfile.read()
+        psw =  getpass('Please enter your password: ')
+        while hashlib.md5(psw.encode('ASCII')).hexdigest() != hashed_psw:
+            psw =  getpass("Wrong Password. Please Try again: ")
+            if psw == 'q':
+                break
+    if hashlib.md5(psw.encode('ASCII')).hexdigest() == hashed_psw:
+        print('got in')
+except FileNotFoundError:
+    with open('.password.txt', 'w+') as pswfile:
+        psw = getpass('Please create a password: ')
+        pswfile.write(hashlib.md5(psw.encode('ASCII')).hexdigest()) 
