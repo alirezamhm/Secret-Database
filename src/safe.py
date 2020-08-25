85677343f18a4c98fb51b41c80222b91172e9169
alirezamhm <alireza.am1379@gmail.com>
2020-08-25 16:59:56 +0430

    store file added

--- src/safe.py
+++ src/safe.py
@@ -1,24 +1,25 @@
 from getpass import getpass
 import hashlib
 import sqlite3
+import base64
+
 
 hashed_psw = ''
 psw = ''
 try:
-    with open('.password.txt', 'r') as pswfile:
+    with open('.password.txt', 'r') as pswfile: # read hashed value of the password from file
         hashed_psw = pswfile.read()
-        psw =  getpass('Please enter your password: ')
+        psw =  getpass('Please enter your password: ') # get password without showing input in terminal
         while hashlib.md5(psw.encode('ASCII')).hexdigest() != hashed_psw:
             psw =  getpass("Wrong Password. Please Try again (press q to exit): ")
             if psw == 'q':
                 break
 except FileNotFoundError:
-    with open('.password.txt', 'w+') as pswfile:
+    with open('.password.txt', 'w+') as pswfile: # store the hash value of password in a file
         psw = getpass('Please create a password: ')
         pswfile.write(hashlib.md5(psw.encode('ASCII')).hexdigest()) 
 
 if hashlib.md5(psw.encode('ASCII')).hexdigest() == hashed_psw:
-    print('in')
     db_conn = sqlite3.connect('safe.db')
     try:
         db_conn.execute('''CREATE TABLE safe (
@@ -30,7 +31,7 @@ if hashlib.md5(psw.encode('ASCII')).hexdigest() == hashed_psw:
     except sqlite3.OperationalError:
         print("You are connected to your safe")
     while True:
-        print('*'*20)
+        print('\n' + '*'*20)
         print('Commands: ')
         print('s: store a file')
         print('o: open a file')
@@ -41,8 +42,28 @@ if hashlib.md5(psw.encode('ASCII')).hexdigest() == hashed_psw:
         if command == 'q':
             break
         elif command == 's':
-            pass
+            FILE_TYPES = {'txt', 'jpg', 'jpeg', 'png'}
+
+            path = input('please enter the full path to your file (example: /home/alireza/pictures/sample.jpg) \n> ')
+            try:
+                full_name = path.split('/')[-1]
+                name, extension = full_name.split('.')
+                data = ''
+                if extension not in FILE_TYPES:
+                    print('Sorry this file type is not supported yet')
+                    continue
+            
+                with open(path, 'rb') as f:
+                    data = base64.b64encode(f.read()).decode() # read data as byte and decode it to string to store
+            except (ValueError, FileNotFoundError):
+                print('This file does not exist')
+                continue
+
+            db_conn.execute('''INSERT INTO safe (FULL_NAME, NAME, EXTENSION, DATA)
+                                VALUES ("{}", "{}", "{}", "{}");'''.format(full_name, name, extension, data))
+            db_conn.commit()
         elif command == 'o':
             pass
         else:
             print("Invalid command")
+    db_conn.close()
